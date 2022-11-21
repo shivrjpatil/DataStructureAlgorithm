39de85a0ddd4390f559164d8a1ed86b5b549e98f
shivrjpatil <shivrajp972@gmail.com>
2022-11-21 16:40:10 +0530

    [shiv]add:folder structure is created

---
+++ DataStructureAlgoritm/BinarySearch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgoritm
+{
+    internal class BinarySearch
+    {
+        string[] binaryArr;
+        public void ReadTextFile(string filePath)
+        {
+            string words = File.ReadAllText(filePath);
+            binaryArr = words.Split(",");
+        }
+        public void BinarySearchOperation(string input)
+        {
+            foreach(var word in binaryArr)
+            {
+                if (word.Equals(input))
+                {
+                    Console.WriteLine("found the word");
+                    return;
+                }
+            }
+            Console.WriteLine("search element is not in available");
+        }
+    }
+}
---
+++ DataStructureAlgoritm/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace DataStructureAlgoritm
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to data structure algoritm");
+            BinarySearch binary=new BinarySearch();
+            binary.ReadTextFile("C:\\Users\\shiv\\Desktop\\git repositary\\DataStructureAlgoritm\\Files");
+            binary.BinarySearchOperation("code");
+
+        }
+    }
+}
