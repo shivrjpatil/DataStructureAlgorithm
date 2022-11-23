57904ce8d0a4e248ac5a367090adc9de9e54250c
shivrjpatil <shivrajp972@gmail.com>
2022-11-23 17:38:17 +0530

    [shiv]add:user input problem

---
+++ DataStructureAlgoritm/UserInput.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgoritm
+{
+    internal class UserInput
+    {
+        public void ReplaceString()
+        {
+            string input = "Hello <<name>>, We have your full name as <<full name>> in our system. your contact number is 91-xxxxxxxxxx.";
+            Console.WriteLine("enter your name");
+            string name=Console.ReadLine();
+            input = input.Replace("<<name>>", name);
+
+            Console.WriteLine("enter your full name");
+            string fullname = Console.ReadLine();
+            input = input.Replace("<<full name>>", fullname);
+
+            Console.WriteLine("enter your mobile number");
+            string mobile = Console.ReadLine();
+            input = input.Replace("xxxxxxxxxx", mobile);
+
+            Console.WriteLine(input);
+        }
+    }
+}
