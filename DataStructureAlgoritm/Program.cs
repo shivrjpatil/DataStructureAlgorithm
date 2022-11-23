e29778de0b6e4cd479806b99a22ed844c5f19d93
shivrjpatil <shivrajp972@gmail.com>
2022-11-23 17:41:24 +0530

    [shiv]add:main method

--- DataStructureAlgoritm/Program.cs
+++ DataStructureAlgoritm/Program.cs
@@ -5,11 +5,46 @@ namespace DataStructureAlgoritm
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("welcome to data structure algoritm");
-            BinarySearch binary=new BinarySearch();
-            binary.ReadTextFile("C:\\Users\\shiv\\Desktop\\git repositary\\DataStructureAlgoritm\\Files");
-            binary.BinarySearchOperation("code");
-
+            bool flag = true;
+            while(flag)
+            { 
+             Console.WriteLine("DATA STRUCTURES AND ALGORITM");
+             Console.WriteLine("enter the option to select");
+             int option=Convert.ToInt32(Console.ReadLine());
+            
+            
+                switch (option)
+                {
+                    case 1:
+                        InsertionSort sort = new InsertionSort();
+                        int[] arr = { 20, 39, 2, 3, 6, 54, 3 };
+                        sort.Sort(arr);
+                        break;
+                    case 2:
+                        BubbleSort bubbleSort = new BubbleSort();
+                        int[] arrr = { 6, 3, 4, 27, 8 };
+                        bubbleSort.Sort(arrr);
+                        break;
+                    case 3:
+                        Anagram check = new Anagram();
+                        check.CheckAnagram("heart", "earth");
+                        break;
+                    case 4:
+                        UserInput input = new UserInput();
+                        input.ReplaceString();
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter the word you want to Search");
+                        BinarySearch binary = new BinarySearch();
+                        binary.ReadTextFile(@"C:\Users\shiv\Desktop\git repositary\DataStructureAlgorithm\DataStructureAlgoritm\Files\BinaySearch.txt");
+                        binary.BinarySearchOperation("hello");
+                        
+                        break;
+                    default:
+                        Console.WriteLine("you have entered wrong option");
+                        break;
+                }
+            }
         }
     }
 }
