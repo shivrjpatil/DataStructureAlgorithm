6a8797662c5d0b22a7be4cc65b36b8748e5fbe83
shivrjpatil <shivrajp972@gmail.com>
2022-11-23 17:26:52 +0530

    [shiv]add:anagram problem

---
+++ DataStructureAlgoritm/Anagram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgoritm
+{
+    internal class Anagram
+    {
+        public void CheckAnagram(string inputStr1,string inputStr2)
+        {
+            string input1 = Test(inputStr1);
+            string input2 = Test(inputStr2);
+            if (input1.Equals(input2))
+                Console.WriteLine("Anagram");
+            else
+                Console.WriteLine("not an anagarm");
+
+        }
+        public string Test(string input)
+        {
+            char[] arr=input.ToCharArray();
+            Array.Sort(arr);
+            return new string(arr);
+        }
+    }
+}
